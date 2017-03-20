337ac4ed6d46acf0489aa4401f54e16e01858a88
Bingxing Wang <i@imbushuo.net>
2017-03-20 08:34:35 -0700

    Address an issue blocks query provider parameters being sent.

--- src/Moegirlpedia.MediaWikiInterop.Primitives/Transform/QueryRequestSerializer.cs
+++ src/Moegirlpedia.MediaWikiInterop.Primitives/Transform/QueryRequestSerializer.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Reflection;
+using Moegirlpedia.MediaWikiInterop.Primitives.Foundation.Query;
 
 namespace Moegirlpedia.MediaWikiInterop.Primitives.Transform
 {
@@ -18,21 +20,33 @@ namespace Moegirlpedia.MediaWikiInterop.Primitives.Transform
 
             return Task.Factory.StartNew<HttpContent>(() =>
             {
-                // Intermediate Key-Value pair
-                var kvPairs = new List<KeyValuePair<string, string>>();
-                // Query providers
-                kvPairs.AddRange(content.m_queryProviders.SelectMany(k => k.ToKeyValuePairCollection()));
-                // Base parameters
-                kvPairs.AddRange(content.ToKeyValuePairCollection());
-
-                // Set Raw continuation to true
-                kvPairs.Add(new KeyValuePair<string, string>("rawcontinue", "1"));
-
-                // Add format config
-                kvPairs.AddRange(JsonFormatConfig.FormatConfig);
-
-                return new FormUrlEncodedContent(kvPairs);
+                return new FormUrlEncodedContent(GetKeyPairs(content));
             }, ctkn);
+        }
+
+        internal List<KeyValuePair<string, string>> GetKeyPairs(QueryInputModel content)
+        {
+            // Intermediate Key-Value pair
+            var kvPairs = new List<KeyValuePair<string, string>>();
+
+            // Query providers
+            kvPairs.AddRange(content.m_queryProviders.SelectMany(k => k.GetParameters()));
+
+            // Query providers meta
+            kvPairs.AddRange(content.m_queryProviders.Select(i => i.GetType().GetTypeInfo().GetCustomAttribute<QueryProviderAttribute>())
+                .GroupBy(i => i.Category)
+                .Select(i => new KeyValuePair<string, string>(i.Key, string.Join("|", i.Select(a => a.Name)))));
+
+            // Base parameters
+            kvPairs.AddRange(content.ToKeyValuePairCollection());
+
+            // Set Raw continuation to true
+            kvPairs.Add(new KeyValuePair<string, string>("rawcontinue", "1"));
+
+            // Add format config
+            kvPairs.AddRange(JsonFormatConfig.FormatConfig);
+
+            return kvPairs;
         }
     }
 }
