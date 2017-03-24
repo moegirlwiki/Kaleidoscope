bdc339393a4dcbdff91c376f2a5a4ff8a1730892
Bingxing Wang <i@imbushuo.net>
2017-03-24 08:12:34 -0700

    Implement logout action.

---
+++ src/Moegirlpedia.MediaWikiInterop.Actions/LogoutAction.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="LogoutAction.cs" company="Project Kaleidoscope Authors">
+// Copyright (c) Moegirlsaikou Foundation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using Microsoft.Extensions.Options;
+using Moegirlpedia.MediaWikiInterop.Actions.Models;
+using Moegirlpedia.MediaWikiInterop.Primitives.Foundation;
+using Moegirlpedia.MediaWikiInterop.Primitives.Foundation.Internals;
+using Moegirlpedia.MediaWikiInterop.Primitives.Transform;
+using System;
+
+namespace Moegirlpedia.MediaWikiInterop.Actions
+{
+    public class LogoutAction : ApiAction<LogoutInputModel>
+    {
+        public override string Name => "logout";
+
+        public LogoutAction(IOptions<EnvironmentOption> envOptions) : base(envOptions) { }
+
+        protected override Func<IRequestSerializer<LogoutInputModel>> SerializerAction =>
+            new Func<IRequestSerializer<LogoutInputModel>>(() =>
+            new FormUrlEncodedContentRequestSerializer<LogoutInputModel>());
+    }
+}
---
+++ src/Moegirlpedia.MediaWikiInterop.Actions/Models/LogoutInputModel.cs
@@ -0,0 +1,15 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="LogoutInputModel.cs" company="Project Kaleidoscope Authors">
+// Copyright (c) Moegirlsaikou Foundation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using Moegirlpedia.MediaWikiInterop.Primitives.Foundation;
+
+namespace Moegirlpedia.MediaWikiInterop.Actions.Models
+{
+    public class LogoutInputModel : IApiActionRequest
+    {
+        // But you don't need to do anything
+    }
+}
